11e3870add0b910762c277a268aa968c347b56db
Caitlin Loftus <caitlinloftus@Caitlins-MacBook-Pro-2.local>
2020-03-03 20:08:30 -0600

    twitter search api almost working, need to sort out language filter

--- twitter/search_bills.py
+++ twitter/search_bills.py
@@ -29,7 +29,7 @@ def find_active_bills(bill_info_file):
 
     return list(active_bills.keys())
 
-def call_twitter_api(bill_query_str):
+def call_twitter_api(bill_query_str, next=False):
     '''
     '''
     twitter_api = 'https://api.twitter.com/1.1/tweets/search/30day/prod.json'
@@ -38,47 +38,18 @@ def call_twitter_api(bill_query_str):
         'authorization': credentials.TWITTER_HEADER_AUTH,
         'content-type': 'application/json',
     }
-
     data = '{{"query": "{} place:Illinois lang:en",\
-                "maxResults":"100"}}'.format(bill_query_str)
+            "maxResults":"100"}}'.format(bill_query_str)
+
+    if next:
+        data = '{{"query": "{} place:Illinois lang:en",\
+                "maxResults":"100", "next"}}'.format(bill_query_str)
 
     response = requests.post(twitter_api, headers=headers, data=data)
 
     response.raise_for_status()
-
     return response.json()
 
-
-def update_tweet_dict(tweet_json, bill_num, tweet_dict):
-    '''
-    '''
-    tweet_dict_keys = ['id', 'text', 'created_at']
-
-    if bill_num not in tweet_dict:
-        tweet_dict[bill_num] = []
-
-    tweet = {key: tweet_json[key] for key in tweet_dict_keys}
-    tweet['user'] = tweet_json['user']['name']
-    if tweet_json['entities']:
-        if tweet_json['entities']['urls']:
-            tweet['url'] = tweet_json['entities']['urls'][0]['url']
-
-    tweet_dict[bill_num].append(tweet)
-
-def update_users_dict(tweet_json, bill_num, users_dict):
-    '''
-    '''
-    users_dict_keys = ['id', 'name','screen_name', 'location', 'url', 'description']
-
-    if bill_num not in users_dict.keys():
-        users_dict[bill_num] = []
-
-    user_dict = {key: tweet_json['user'][key] for key in users_dict_keys}
-    users_dict[bill_num].append(user_dict)
-
-    if 'retweeted_status' in tweet_json.keys():
-        rt_user_dict = {key: tweet_json['retweeted_status']['user'][key] for key in users_dict_keys}
-        users_dict[bill_num].append(rt_user_dict)
 
 def search_bills(bill_info_file):
     '''
@@ -87,15 +58,66 @@ def search_bills(bill_info_file):
     users_dict = {}
     bill_nums = find_active_bills(bill_info_file)
 
-    for i, b in enumerate(bill_nums[:100]):
+    for i, b in enumerate(bill_nums[:4]):
         if i != 0 and i % 12 == 0:  # rate limit = 12 rpm
+            print('sleeping...')
             time.sleep(60)
-        query_bill_str = '{} OR #{} '.format(b, b)
-        tweets = call_twitter_api(query_bill_str)
 
-        if tweets['results']:
-            for t in tweets['results']:
-                update_tweet_dict(t, b, tweet_dict)
-                update_users_dict(t, b, users_dict)
+        query_bill_str = '{} OR #{} '.format(b, b)
+
+        tweets = call_twitter_api(query_bill_str)
+        get_data_from_tweet(tweets, b, tweet_dict, users_dict)
+
+        while 'next' in tweets:
+            tweets = call_twitter_api(query_bill_str)
+            get_data_from_tweet(tweets, b, tweet_dict, users_dict)
 
     return (tweet_dict, users_dict)
+
+
+def get_data_from_tweet(tweets, bill_num, tweet_dict, users_dict):
+    '''
+    '''
+    if tweets['results']:
+        for t in tweets['results']:
+            update_tweet_dict(t, bill_num, tweet_dict)
+            update_users_dict(t, bill_num, users_dict)
+
+def update_tweet_dict(tweet_json, bill_num, tweet_dict):
+    '''
+    '''
+
+    if bill_num not in tweet_dict:
+        tweet_dict[bill_num] = {}
+
+    tweet_id = tweet_json['id']
+    if tweet_id not in tweet_dict[bill_num]:
+        tweet = {key: tweet_json[key] for key in ['text', 'created_at']}
+        tweet['user'] = tweet_json['user']['name']
+
+    if tweet_json['entities']:
+        if tweet_json['entities']['urls']:
+            tweet['url'] = tweet_json['entities']['urls'][0]['url']
+
+    tweet_dict[bill_num][tweet_id] = tweet
+
+
+def update_users_dict(tweet_json, bill_num, users_dict):
+    '''
+    '''
+    users_dict_keys = ['name','screen_name', 'location', 'url', 'description']
+
+    if bill_num not in users_dict:
+        users_dict[bill_num] = {}
+
+    user_id = tweet_json['user']['id']
+    if user_id not in users_dict[bill_num]:
+        user_dict = {key: tweet_json['user'][key] for key in users_dict_keys}
+        user_dict['count'] = 1
+        users_dict[bill_num][user_id] = user_dict
+    else:
+        users_dict[bill_num][user_id]['count'] += 1
+
+# should we use tag 'is_verified' - will that give us just organisations?
+
+# do we want to include retweet users?
