ac0a4bbfe354f072a9b094bbfda399738963ca1f
Caitlin Loftus <caitlinloftus@Caitlins-MacBook-Pro-2.local>
2020-03-03 18:24:52 -0600

    only look up bills where last action is in past 2 months

--- twitter/search_bills.py
+++ twitter/search_bills.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import time
+from datetime import datetime
 
 import credentials
 
 def find_active_bills(bill_info_file):
     '''
-    Generate list of bill numbers for bills that are active (have a committee)
+    Generate list of bill numbers for bills that are active (have a committee
+    and last date is within the past month)
 
     Inputs:
         bill_json_file (str): location of bill_info json file
@@ -17,19 +20,14 @@ def find_active_bills(bill_info_file):
         json_str = bi.read()
         bill_dict = json.loads(json_str)
 
+    date_since = datetime.strptime('1/1/2020', '%m/%d/%Y')
+
     active_bills = {b: bill_dict[b]['short description'] for b in bill_dict
-                       if bill_dict[b]['committee']!=None}
+                       if bill_dict[b]['committee']!=None and
+                            datetime.strptime(bill_dict[b]['last action']['date'],
+                                              '%m/%d/%Y') >= date_since}
 
     return list(active_bills.keys())
-
-def generate_bill_call_string(bill_block):
-    '''
-    '''
-    assert len(bill_blocks) <= 5
-
-    query_bills = bill_blocks + ['#' + x for x in bill_block]
-
-    return ' OR '.join(query_bills)
 
 def call_twitter_api(bill_num_list, bill_query_str):
     '''
@@ -42,60 +40,60 @@ def call_twitter_api(bill_num_list, bill_query_str):
     }
 
     data = '{{"query": "{} place: Illinois lang:en",\
-                "maxResults":"100",\
-                "fromDate":"202002010000",\
-                "toDate":"202002270000"}}'.format(bill_query_str)
+                "maxResults":"100"}}'.format(bill_query_str)
 
     response = requests.post(twitter_api, headers=headers, data=data)
 
+    print(response.ok)
+    response.raise_for_status()
+
     response_output = response.json()
 
-    while 'next' in response_output:
-        response = requests.post(twitter_api, headers=headers, data=data)
+
+def update_tweet_dict(tweet_json, bill_num, tweet_dict):
+    '''
+    '''
+    tweet_dict_keys = ['id', 'text', 'created_at']
+
+    if bill_num not in tweet_dict:
+        tweet_dict[bill_num] = []
+
+    tweet = {key: tweet_json[key] for key in tweet_dict_keys}
+    tweet['user'] = tweet_json['user']['name']
+    if tweet_json['entities']['urls']:
+        tweet['url'] = tweet['entities']['urls'][0]['url']
+
+    tweet_dict[bill_num].append(tweet)
+
+def update_users_dict(tweet_json, bill_num, users_dict):
+    '''
+    '''
+    users_dict_keys = ['id', 'name','screen_name', 'location', 'url', 'description']
+
+    if bill_num not in users_dict.keys():
+        users_dict[bill_num] = []
+
+    user_dict = {key: tweet['user'][key] for key in users_dict_keys}
+    users_dict[bill_num].append(user_dict)
+
+    if 'retweeted_status' in tweet.keys():
+        rt_user_dict = {key: tweet['retweeted_status']['user'][key] for key in users_dict_keys}
+        users_dict[bill_num].append(rt_user_dict)
 
 def search_bills(bill_info_file):
     '''
     '''
-    bill_nums = find_active_bills(bill_info_file)
-
-    for i in range(0, len(bill_nums), 5):
-        bill_num_block = bill_nums[i: i+5]
-        bill_query_str = generate_bill_call_string(bill_num_block)
-        time.sleep(1/20)
-        tweets = call_twitter_api(bill_query_str)
-
-
-def update_tweet_dict(response_json):
-    '''
-    '''
-    for tweet in response_output['results']:
-        id = tweet['id']
-        tweet_dict[id] = {}
-        tweet_dict[id]['text'] = tweet['text']
-        tweet_dict[id]['user'] = tweet['user']['name']
-        tweet_dict[id]['bill'] = bill_num
-        tweet_dict[id]['timestamp'] = tweet['created_at']
-        if tweet['entities']['urls']:
-            tweet_dict[id]['url'] = tweet['entities']['urls'][0]['url']
-
-
-        if bill_num not in users_dict.keys():
-            users_dict[bill_num] = []
-        user_dict = {key: tweet['user'][key] for key in users_dict_keys}
-        users_dict[bill_num].append(user_dict)
-
-        if 'retweeted_status' in tweet.keys():
-            rt_user_dict = {key: tweet['retweeted_status']['user'][key] for key in users_dict_keys}
-            users_dict[bill_num].append(rt_user_dict)
-
-def go(bill_info_file):
-    '''
-    '''
     tweet_dict = {}
     users_dict = {}
+    bill_nums = find_active_bills(bill_info_file)
 
+    for i, b in enumerate(bill_nums[:4]):
+        if i % 12 == 0:  # rate limit = 12 rpm
+            time.sleep(60)
+        query_bill_str = '{} OR #{} '.format(b, b)
+        tweets = call_twitter_api(query_bill_str)
 
-
-
-
-users_dict_keys = ['id', 'name','screen_name', 'location', 'url', 'description']
+        if tweetss['results']:
+            for t in tweets['results']:
+                update_tweet_dict(t, b, tweet_dict)
+                update_users_dict(t, b, users_dict)
