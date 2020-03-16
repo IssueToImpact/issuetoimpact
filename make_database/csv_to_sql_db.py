0ea30ef3ac3afd4f8038785dcb3e7ab7f976af18
akwhaley <amandakristenwhaley@gmail.com>
2020-03-15 20:58:44 -0500

    change in make_database

--- make_database/csv_to_sql_db.py
+++ make_database/csv_to_sql_db.py
@@ -61,7 +61,7 @@ def make_database(db_name):
     '''
     read_bills = pd.read_csv('./data/bill_table_data.csv', header = 0)
     read_bill_keywords = pd.read_csv('./data/bill_keywords.csv')
-    read_reps = pd.read_csv('./data/rep_data.csv', header = 0, usecols = range(1,24))
+    read_reps = pd.read_csv('./data/rep_data.csv', header = 0, usecols = range(0,24))
     read_tweets = pd.read_csv('./data/tweets.csv', header = 0)
 
     conn = sqlite3.connect(db_name)
