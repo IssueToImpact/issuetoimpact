085b507fa4f1e09ab1368c1d521be1fbdd0fd44a
elnelson575 <elizabethwnelson5@gmail.com>
2020-03-27 13:03:17 -0400

    Changed settings

--- itoiUI/itoiUI/settings.py
+++ itoiUI/itoiUI/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
